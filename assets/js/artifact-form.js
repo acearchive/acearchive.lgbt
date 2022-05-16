715ab84bb02d03e21318d45b996c9b26f70bf9e9
Frawley <frawleyskid@gmail.com>
2022-05-15 23:18:32 -0400

    Support adding multiple files and links

---
+++ assets/js/artifact-form.js
@@ -0,0 +1,71 @@
+import showdown from "showdown";
+import { schema } from "@params";
+
+const form = document.querySelector(".artifact-form main form");
+
+const nameToId = (fieldName) => fieldName.replace(".", "-")
+
+let mdConverter = new showdown.Converter();
+
+const createFieldItem = (field, showHelp) => {
+  const fieldItem = document.createElement("div");
+  fieldItem.classList.add("field-item", "card", "card-body");
+
+  for (const fieldName of field.fields) {
+    const nestedField = field.definitions[fieldName];
+    fieldItem.appendChild(createFormGroup(nestedField, showHelp));
+  }
+
+  return fieldItem;
+}
+
+const createFormGroup = (field, showHelp = true) => {
+  const fieldId = nameToId(field.name);
+
+  const formGroup = document.createElement("div");
+  formGroup.id = `form-group-${fieldId}`;
+  formGroup.classList.add("form-group");
+
+  if (field.fields && field.fields.length > 0) {
+    formGroup.innerHTML = `
+      <div id="form-group-${fieldId}" class="mb-3">
+        <label for="field-item-container-${fieldId}" class="form-label">${field.label}</label>
+        <div class="field-help form-text">${mdConverter.makeHtml(field.description)}</div>
+        <div id="field-item-container-${fieldId}" class="field-item-container"></div>
+        <button class="btn btn-primary btn-sm mt-2 add-field-item" type="button">Add ${field.singular}</button>
+      </div>
+    `
+
+    formGroup.querySelector("button.add-field-item").addEventListener("click", () => {
+      const itemContainer = formGroup.querySelector(".field-item-container")
+      const isFirstItemInContainer = itemContainer.childElementCount === 0;
+      itemContainer.appendChild(createFieldItem(field, isFirstItemInContainer));
+    })
+  } else {
+    formGroup.innerHTML = `
+      <div id="form-group-${fieldId}" class="mb-3">
+        <label for="field-input-${fieldId}" class="form-label">${field.label}</label>
+        <input type="text" class="form-control" id="field-input-${fieldId}" aria-describedby="field-help-${fieldId}">
+        <div class="invalid-feedback"></div>
+        <div id="field-help-${fieldId}" class="field-help form-text">${mdConverter.makeHtml(field.description)}</div>
+      </div>
+    `
+
+    if (field.required) {
+      formGroup.querySelector("input").setAttribute("required", "true");
+    }
+  }
+
+  if (!showHelp) {
+    formGroup.querySelector(".field-help").remove();
+  }
+
+  return formGroup;
+}
+
+if (form) {
+  for (const fieldName of schema.fields) {
+    const field = schema.definitions[fieldName];
+    form.appendChild(createFormGroup(field))
+  }
+}
