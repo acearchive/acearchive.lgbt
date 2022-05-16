7bd321be355bfabbeb6ac69206543937db78c3c2
Frawley <frawleyskid@gmail.com>
2022-05-16 13:21:16 -0400

    Remove duplicate code

--- assets/js/artifact-form.js
+++ assets/js/artifact-form.js
@@ -54,27 +54,23 @@ const createFormGroup = (field, showHelp = true) => {
   formGroup.classList.add("form-group");
 
   if (field.fields && field.fields.length > 0) {
-    if (showHelp) {
-      formGroup.innerHTML = `
-        <label for="field-item-container-${fieldId}" class="form-label">${field.label}</label>
-        <div class="field-help form-text">${mdConverter.makeHtml(field.description)}</div>
-        <div id="field-item-container-${fieldId}" class="field-item-container"></div>
-        <button class="btn mt-2 add-field-item form-button" type="button">
-          <span class="me-1" aria-hidden="true">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
-              <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-            </svg>
-          </span>
-          <span>Add ${field.singular}</span>
-        </button>
-      `;
-    } else {
-      formGroup.innerHTML = `
-        <label for="field-item-container-${fieldId}" class="form-label">${field.label}</label>
-        <div id="field-item-container-${fieldId}" class="field-item-container"></div>
-        <button class="btn btn-primary btn-sm mt-2 add-field-item" type="button">Add ${field.singular}</button>
-      `;
+    formGroup.innerHTML = `
+      <label for="field-item-container-${fieldId}" class="form-label">${field.label}</label>
+      <div class="field-help form-text">${mdConverter.makeHtml(field.description)}</div>
+      <div id="field-item-container-${fieldId}" class="field-item-container"></div>
+      <button class="btn add-field-item form-button" type="button">
+        <span class="me-1" aria-hidden="true">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+          </svg>
+        </span>
+        <span>Add ${field.singular}</span>
+      </button>
+    `;
+
+    if (!showHelp) {
+      formGroup.querySelector(".field-help").remove();
     }
 
     formGroup.querySelector("button.add-field-item").addEventListener("click", () => {
