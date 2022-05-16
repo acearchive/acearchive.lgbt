e9775281db18d07ef1975daf8f83332fcd19f91b
Frawley <frawleyskid@gmail.com>
2022-05-16 13:14:44 -0400

    Restyle form button

--- assets/js/artifact-form.js
+++ assets/js/artifact-form.js
@@ -13,8 +13,7 @@ const createFieldItem = (field, itemsInContainer) => {
   fieldItem.innerHTML = `
     <span class="field-item-gutter">
       <span class="field-item-index">#${itemsInContainer + 1}</span>
-      <button type="button" class="field-item-action field-item-delete btn">
-        <span class="visually-hidden">Delete</span>
+      <button type="button" class="field-item-action field-item-delete btn" aria-label="Delete">
         <span aria-hidden="true">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
@@ -57,20 +56,24 @@ const createFormGroup = (field, showHelp = true) => {
   if (field.fields && field.fields.length > 0) {
     if (showHelp) {
       formGroup.innerHTML = `
-        <div id="form-group-${fieldId}" class="mb-3">
-          <label for="field-item-container-${fieldId}" class="form-label">${field.label}</label>
-          <div class="field-help form-text">${mdConverter.makeHtml(field.description)}</div>
-          <div id="field-item-container-${fieldId}" class="field-item-container"></div>
-          <button class="btn btn-primary btn-sm mt-2 add-field-item" type="button">Add ${field.singular}</button>
-        </div>
+        <label for="field-item-container-${fieldId}" class="form-label">${field.label}</label>
+        <div class="field-help form-text">${mdConverter.makeHtml(field.description)}</div>
+        <div id="field-item-container-${fieldId}" class="field-item-container"></div>
+        <button class="btn mt-2 add-field-item form-button" type="button">
+          <span class="me-1" aria-hidden="true">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+              <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+            </svg>
+          </span>
+          <span>Add ${field.singular}</span>
+        </button>
       `;
     } else {
       formGroup.innerHTML = `
-        <div id="form-group-${fieldId}" class="mb-3">
-          <label for="field-item-container-${fieldId}" class="form-label">${field.label}</label>
-          <div id="field-item-container-${fieldId}" class="field-item-container"></div>
-          <button class="btn btn-primary btn-sm mt-2 add-field-item" type="button">Add ${field.singular}</button>
-        </div>
+        <label for="field-item-container-${fieldId}" class="form-label">${field.label}</label>
+        <div id="field-item-container-${fieldId}" class="field-item-container"></div>
+        <button class="btn btn-primary btn-sm mt-2 add-field-item" type="button">Add ${field.singular}</button>
       `;
     }
 
@@ -81,24 +84,20 @@ const createFormGroup = (field, showHelp = true) => {
   } else {
     if (showHelp) {
       formGroup.innerHTML = `
-        <div id="form-group-${fieldId}" class="mb-3">
-          <label for="field-input-${fieldId}" class="form-label">${field.label}</label>
-          <input type="${field.inputType}" class="form-control" id="field-input-${fieldId}" aria-describedby="field-help-${fieldId}" placeholder="${field.placeholder}">
-          <div class="invalid-feedback"></div>
-          <div id="field-help-${fieldId}" class="field-help form-text">${mdConverter.makeHtml(field.description)}</div>
-        </div>
+        <label for="field-input-${fieldId}" class="form-label">${field.label}</label>
+        <input type="${field.inputType}" class="form-control" id="field-input-${fieldId}" aria-describedby="field-help-${fieldId}" placeholder="${field.placeholder}">
+        <div class="invalid-feedback"></div>
+        <div id="field-help-${fieldId}" class="field-help form-text">${mdConverter.makeHtml(field.description)}</div>
       `
     } else {
       formGroup.innerHTML = `
-        <div id="form-group-${fieldId}">
-          <div class="row">
-            <label for="field-input-${fieldId}" class="col-sm-3 col-form-label">${field.label}</label>
-            <div class="col">
-              <input type="${field.inputType}" class="form-control" id="field-input-${fieldId}" aria-describedby="field-help-${fieldId}" placeholder="${field.placeholder}">
-            </div>
+        <div class="row">
+          <label for="field-input-${fieldId}" class="col-sm-3 col-form-label">${field.label}</label>
+          <div class="col">
+            <input type="${field.inputType}" class="form-control" id="field-input-${fieldId}" aria-describedby="field-help-${fieldId}" placeholder="${field.placeholder}">
           </div>
-          <div class="invalid-feedback"></div>
         </div>
+        <div class="invalid-feedback"></div>
       `;
     }
 
