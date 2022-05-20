8316e9fee1e1f0fb04dbff4df8ffb988d68bba6e
Frawley <frawleyskid@gmail.com>
2022-05-19 23:19:47 -0400

    Fix bug validating subdomain gateway URLs

--- assets/js/artifact-form.js
+++ assets/js/artifact-form.js
@@ -222,7 +222,7 @@ const createFieldItemFormGroup = (field, fieldItemIndex = 0) => {
             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
           </svg>
         </span>
-        <span>Add ${field.singularLabel}</span>
+        Add ${field.singularLabel}
       </button>
     `;
 
