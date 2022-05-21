8078ac65b695e34ac1ccd2a34157345383dad075
Frawley <frawleyskid@gmail.com>
2022-05-21 18:50:49 -0400

    Fix popup blocker breaking the artifact form

--- assets/js/artifact-form/submit.js
+++ assets/js/artifact-form/submit.js
@@ -1,7 +1,8 @@
 import yaml from "js-yaml";
 import { schema } from "@params";
-import { checkValidity } from "./validate";
 import { readFormData } from "./data";
+import { fieldNameForInputElement, getQueryParams } from "./util";
+import { runValidator } from "./validate";
 
 const groupedFieldKeys = (() => {
   const keysOfField = (field) => {
@@ -59,19 +60,144 @@ const artifactFormSubmitUrl = (form) => {
   return githubPrSubmitUrl(urlSlug, serializeFormDataToMarkdownFrontMatter(data));
 };
 
+const buttonValidateText = "Validate";
+const buttonLoadingText = "Validating...";
+const buttonSubmitText = "Submit";
+const labelValidateText = "Check that everything looks correct.";
+const labelLoadingText = "Checking that everything looks correct.";
+const labelSubmitText = "Open a pull request on GitHub.";
+
+const setButtonLoading = (submitButton) => {
+  const buttonElement = submitButton.querySelector("button");
+  const labelElement = submitButton.querySelector("label");
+  const submitButtonChild = submitButton.querySelector(":scope > *");
+
+  buttonElement.disabled = true;
+  buttonElement.innerHTML = `
+    <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+    ${buttonLoadingText}
+  `;
+  labelElement.innerText = labelLoadingText;
+  submitButtonChild.classList.remove("is-invalid", "is-valid");
+};
+
+const setButtonSubmit = (submitButton) => {
+  const buttonElement = submitButton.querySelector("button");
+  const labelElement = submitButton.querySelector("label");
+  const submitButtonChild = submitButton.querySelector(":scope > *");
+
+  buttonElement.disabled = false;
+  buttonElement.innerText = buttonSubmitText;
+  labelElement.innerText = labelSubmitText;
+  submitButtonChild.classList.remove("is-invalid");
+  submitButtonChild.classList.add("is-valid");
+};
+
+const setButtonNeedsValidation = (submitButton) => {
+  const buttonElement = submitButton.querySelector("button");
+  const labelElement = submitButton.querySelector("label");
+  const submitButtonChild = submitButton.querySelector(":scope > *");
+
+  buttonElement.disabled = false;
+  buttonElement.innerText = buttonValidateText;
+  labelElement.innerText = labelValidateText;
+  submitButtonChild.classList.remove("is-valid", "is-invalid");
+};
+
+const setButtonInvalid = (submitButton) => {
+  const buttonElement = submitButton.querySelector("button");
+  const labelElement = submitButton.querySelector("label");
+  const submitButtonChild = submitButton.querySelector(":scope > *");
+
+  buttonElement.disabled = false;
+  buttonElement.innerText = buttonValidateText;
+  labelElement.innerText = labelValidateText;
+  submitButtonChild.classList.remove("is-valid");
+  submitButtonChild.classList.add("is-invalid");
+};
+
+const setFormInputsDisabled = (form, disabled) => {
+  for (const buttonElement of form.querySelectorAll(".form-button")) {
+    buttonElement.disabled = disabled;
+  }
+
+  for (const inputElement of form.querySelectorAll(".form-field input")) {
+    if (
+      fieldNameForInputElement(inputElement) === schema.definitions.slug.fieldName &&
+      getQueryParams().modify !== undefined
+    ) {
+      continue;
+    }
+
+    inputElement.disabled = disabled;
+  }
+};
+
+const showValidityMessages = (form) => {
+  for (const validationElement of form.querySelectorAll(".needs-validation")) {
+    const inputElement = validationElement.querySelector("input");
+
+    if (!inputElement.validity.valid || inputElement.value.length > 0) {
+      // Don't show the validity of the input if it is empty and not required.
+      validationElement.classList.add("was-validated");
+    }
+  }
+};
+
+const registerSubmitHandler = (form, submitButton) => {
+  let formIsValidated = false;
+
+  const submitEventHandler = () => {
+    window.open(artifactFormSubmitUrl(form), "_blank");
+  };
+
+  const validateEventHandler = () => {
+    setButtonLoading(submitButton);
+    setFormInputsDisabled(form, true);
+
+    runValidator(form).then(() => {
+      setFormInputsDisabled(form, false);
+      showValidityMessages(form);
+      formIsValidated = true;
+
+      if (form.checkValidity()) {
+        setButtonSubmit(submitButton);
+        buttonHandler = submitEventHandler;
+      } else {
+        setButtonInvalid(submitButton);
+        buttonHandler = validateEventHandler;
+      }
+    });
+  };
+
+  let buttonHandler = validateEventHandler;
+
+  for (const inputElement of form.querySelectorAll(".form-field input")) {
+    inputElement.addEventListener("input", () => {
+      if (formIsValidated) {
+        setButtonNeedsValidation(submitButton);
+        buttonHandler = validateEventHandler;
+        formIsValidated = false;
+      }
+    });
+  }
+
+  submitButton.querySelector("button").addEventListener("click", () => buttonHandler());
+};
+
 export const createSubmitButton = (form) => {
   const submitButton = document.createElement("div");
   submitButton.classList.add("submit-control");
   submitButton.innerHTML = `
-    <button id="artifact-form-submit-button" class="btn btn-primary me-1" type="button">Submit</button>
-    <label class="form-text" for="artifact-form-submit-button">Open a pull request on GitHub</label>
+    <div>
+      <button id="artifact-form-submit-button" class="btn btn-primary me-1" type="button" aria-describedby="artifact-form-submit-help">${buttonValidateText}</button>
+      <label class="form-text" for="artifact-form-submit-button">${labelValidateText}</label>
+    </div>
+    <div id="artifact-form-submit-help" class="valid-feedback">Everything looks good!</div>
+    <div id="artifact-form-submit-help" class="invalid-feedback">Looks like there were some problems.</div>
   `;
 
-  submitButton.querySelector("button").addEventListener("click", () => {
-    checkValidity(form).then((isValid) => {
-      if (isValid) window.open(artifactFormSubmitUrl(form), "_blank");
-    });
-  });
+  registerSubmitHandler(form, submitButton);
 
   return submitButton;
 };
